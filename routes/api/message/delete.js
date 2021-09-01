666a9f0365b574bfc0d763486c534b0c9d8c08eb
บัณฑิต คุ้มสวัสดิ์ <85699339+bandiddeaware@users.noreply.github.com>
2021-09-01 09:56:25 +0700

    status code 400

--- routes/api/message/delete.js
+++ routes/api/message/delete.js
@@ -7,6 +7,8 @@ router.delete('/', auth.required, async function(req, res, next) {
   const result = await add_message(req.body)
   if (result.isError === false){
     return res.status(200).json({ result: result.data, status: true })
+  } else {
+    return res.status(400).json({ result: result.data, status: true })
   }
 })
 module.exports = router;
