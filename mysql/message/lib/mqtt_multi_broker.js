a2de412fef3a233f930b7b3d50f5ca937616419d
บัณฑิต คุ้มสวัสดิ์ <85699339+bandiddeaware@users.noreply.github.com>
2021-09-13 11:20:34 +0700

    set connection timeout.

--- mysql/message/lib/mqtt_multi_broker.js
+++ mysql/message/lib/mqtt_multi_broker.js
@@ -11,8 +11,8 @@ module.exports = async (mqtt_) => {
         clientId: process.env["MQTTCLIENTID_POINT_" + q.broker] + "_" + Math.random().toString(16).substr(2, 8),
         username: process.env["MQTTUSER_POINT_" + q.broker],
         password: process.env["MQTTPASS_POINT_" + q.broker],
-        keepalive: 60,
-        reconnectPeriod: 1000,
+        keepalive: 0,
+        reconnectPeriod: 0,
         protocolId: 'MQIsdp',
         protocolVersion: 3,
         clean: true,
@@ -20,10 +20,26 @@ module.exports = async (mqtt_) => {
       })
 
       return new Promise( (resolve, reject) => {
+
+        var connect_timer = setTimeout(() => {
+          resolve({
+            assambly_id: q.broker,
+            name: `Assambly_Point_${q.broker}`,
+            status: false,
+            message: "can't connect to broker."
+          })
+          console.log("=========================")
+          console.log(`>> Offline << [Assambly_Point_${q.broker}]\n[Time: ${new Date()}]\nTopic: ${q.topic}\nMessage: ${q.message}\nstatus: "unsuccess"`)
+          console.log("=========================")
+        }, process.env.MQTTTIMOUT);
+
         clientMQTT.on('connect', () => {
+          clearTimeout(connect_timer)
           clientMQTT.publish(q.topic, q.message)
           clientMQTT.end()
-          console.log(`[Assambly_Point_${q.broker}]\t[Time: ${new Date()}]\t\tTopic: ${q.topic}\t\tMessage: ${q.message}\tstatus: "success"`)
+          console.log("=========================")
+          console.log(`>> Online << [Assambly_Point_${q.broker}]\n[Time: ${new Date()}]\nTopic: ${q.topic}\nMessage: ${q.message}\nstatus: "success"`)
+          console.log("=========================")
         })
         clientMQTT.on('end', function(){
           resolve( {
@@ -34,7 +50,9 @@ module.exports = async (mqtt_) => {
           })
         })
         clientMQTT.on('error', function(){
-          console.log(`[Assambly_Point_${q.broker}]\t[Time: ${new Date()}]\t\tTopic: ${q.topic}\t\tMessage: ${q.message}\tstatus: "unsuccess"`)
+          console.log("=========================")
+          console.log(`>> Online << [Assambly_Point_${q.broker}]\n[Time: ${new Date()}]\nTopic: ${q.topic}\nMessage: ${q.message}\nstatus: "unsuccess"`)
+          console.log("=========================")
           clientMQTT.end()
           resolve({
             assambly_id: q.broker,
